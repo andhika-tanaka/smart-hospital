2560b6e8b76d92916044b99c786d1746066ffa64
andhika-tanaka <andhika.tanaka@gmail.com>
2020-01-12 20:03:42 +0800

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
+ $DATABASE_URL = parse_url('postgres://mzkbdaajtzaskh:e5006c37586492bdee549e64584888264ad8aaa0a3747e35251ab1090e98965f@ec2-174-129-33-84.compute-1.amazonaws.com:5432/ddulu9kd0br9nt');
 
 use Illuminate\Support\Str;
 
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -14,9 +16,10 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
+   
     'default' => env('DB_CONNECTION', 'pgsql'),
 
+    
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -65,12 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
